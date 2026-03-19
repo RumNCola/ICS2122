1592c195521a2e9a75b6d6bca152bfb67a75099c
RumNCola <jara.fher@gmail.com>
2026-03-19 19:02:26 -0300

    orden

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,14 @@
 # Archivo con información NO SENSIBLE asociada a prametros de la ejecución. Ejemplo: Mipgaps.
 # Notar que la informacion sensible va en environment/.env
+import os
+
+#RUTAS DE CARPETAS IMPORTANTES
+DATA_FOLDER = 'data'
+
+# Nombre de las carpetas de instancias
+DATA_SRC = [
+    os.path.join(DATA_FOLDER, 'Instancia Tipo I'),
+    os.path.join(DATA_FOLDER, 'Instancia Tipo II'),
+    os.path.join(DATA_FOLDER, 'Instancia Tipo III'),
+    os.path.join(DATA_FOLDER, 'Instancia Tipo IV')
+    ]
--- main.bat
+++ main.bat
@@ -1,1 +1,17 @@
-@REM Este módulo será el encargado de ejecutar el programa principal, cargando las variables del entorno.
+@echo off
+:: Este módulo será el encargado de ejecutar el programa principal, cargando las variables del entorno.
+
+:: instalacion de requirements si corresponde
+:: pip install -r requirements.txt
+
+:: Activación de variables de entorno
+call .venv\Scripts\activate.bat
+
+:: Cargar variables de entorno desde .env
+for /f "delims=" %%x in (.env) do (set "%%x")
+
+:: Ejecutar el programa principal
+python main.py
+
+:: Pausa para ver resultados
+pause
