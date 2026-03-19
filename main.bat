4e605974aa434c59887ea05baf69b9e4270a90b8
RumNCola <jara.fher@gmail.com>
2026-03-19 19:05:11 -0300

    'bat'

--- main.bat
+++ main.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Este módulo será el encargado de ejecutar el programa principal, cargando las variables del entorno.
 
-:: instalacion de requirements si corresponde
+:: instalacion de requirements si corresponde Descomentar si es la primera ejecución
 :: pip install -r requirements.txt
 
 :: Activación de variables de entorno
