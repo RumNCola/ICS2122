7018b343a63c3e3365bb272a35f65ae155aec7a1
cristobal ramos <cristbal.ramos@uc.cl>
2026-05-06 21:27:57 -0400

    dist manhattan, rm dist euc

--- src/core.py
+++ src/core.py
@@ -52,7 +52,7 @@ class EstadoCamion:
 
 #------- Helpers geométricos ---------
 def distancia(p1: Tuple[float, float], p2: Tuple[float, float]) -> float:
-    return math.hypot(p1[0] - p2[0], p1[1] - p2[1]) #distancia euclidiana
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]) #distancia manhattan
 
 
 def tiempo_viaje(p1: Tuple[float, float], p2: Tuple[float, float],
